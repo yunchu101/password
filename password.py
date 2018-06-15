032957b03f6d734c817f0508cec9c959256a6d7d
Tina Chen <yunchu101@gmail.com>
2018-06-15 14:28:33 +0800

    change to while x>0

--- password.py
+++ password.py
@@ -1,5 +1,6 @@
 password = 'a123456'
 x = 3
+
 while x > 0:
     answer = input ('password:')
     if answer == password:
@@ -7,5 +8,9 @@ while x > 0:
        break
     else:
         x = x - 1
-        print('密碼錯誤！還有',x,'次機會')
-    
+        print('wrong password, you have', x, 'more chance')
+       
+
+
+
+
